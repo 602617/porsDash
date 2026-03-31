9136a755b844a166c2a4e1a44dbceb8d3f58999f
Martin <martin.lenes@lyse.net>
2026-03-31 23:22:22 +0200

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -16,12 +16,12 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/icon-192.png',
+            src: '/icon-192-v2.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icon-512.png',
+            src: '/icon-512-v2.png',
             sizes: '512x512',
             type: 'image/png'
           },
